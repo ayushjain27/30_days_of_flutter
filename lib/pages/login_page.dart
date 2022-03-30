ed708d94ebf33a54ed4f71718634e538340a6437
ayushjain27 <ayushjain201574@gmail.com>
2022-03-30 21:09:27 +0530

    day 4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,16 +4,46 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 20.0),
+            const Text("Welcome",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                )),
+            const SizedBox(height: 20.0),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "Enter Username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        hintText: "Enter Password", labelText: "Password"),
+                  ),
+                  const SizedBox(height: 20.0),
+                  ElevatedButton(
+                    child: const Text("Login"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Hi Codepur");
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
