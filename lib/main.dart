50991b8ef71bddf6f6b8b08a08cc02ca20765233
ayushjain27 <ayushjain201574@gmail.com>
2022-04-17 09:49:29 +0530

    day-10 completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_catalogue/pages/home_page.dart';
 import 'package:flutter_catalogue/pages/login_page.dart';
 import 'package:flutter_catalogue/utils/routes.dart';
 import 'package:flutter_catalogue/widgets/themes.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,14 +3,32 @@ import 'package:flutter/material.dart';
 import '../widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
-  final days = 30;
-  final name = "Ayush";
+  final int days = 30;
+  final String name = "Ayush";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
+      // body: Container(
+      //     height: 100,
+      //     width: 100,
+      //     color: Colors.green,
+      // ),
+
+      // body: Container(
+      //   constraints: BoxConstraints(
+      //     minHeight: 70,
+      //     maxHeight: 200,
+      //     minWidth: 70,
+      //     maxWidth: 200),
+      //   child: Container(
+      //     height: 100,
+      //     width: 100,
+      //     color: Colors.red,
+      //   ),
+      // ),
       body: Center(
         child: Container(
           child: Text("Welcome to $days days of flutter by $name"),
