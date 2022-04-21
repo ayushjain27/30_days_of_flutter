242ae27ea33d362f04e6bf6fc4fe83c6cb23ad37
ayushjain27 <ayushjain201574@gmail.com>
2022-04-21 22:05:19 +0530

    day-12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -69,21 +69,63 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                // itemCount: dummyList.length,
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(
+                            item.name,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        child: Image.network(item.image),
+                        footer: Container(
+                          child: Text(
+                            item.price.toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ));
+                },
                 itemCount: CatalogModel.items.length,
-                // itemBuilder: (context, index) {
-                itemBuilder: (context, index) =>
-                    ItemWidget(item: CatalogModel.items[index]),
-                // return ItemWidget(
-                //   item: dummyList[index],
-                // );
-                // return ItemWidget(item: CatalogModel.items[index]);
-                // },
               )
             : Center(
                 child: CircularProgressIndicator(),
               ),
+        // child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+        //     ? ListView.builder(
+        //         // itemCount: dummyList.length,
+        //         itemCount: CatalogModel.items.length,
+        //         // itemBuilder: (context, index) {
+        //         itemBuilder: (context, index) =>
+        //             ItemWidget(item: CatalogModel.items[index]),
+        //         // return ItemWidget(
+        //         //   item: dummyList[index],
+        //         // );
+        //         // return ItemWidget(item: CatalogModel.items[index]);
+        //         // },
+        //       )
+        //     : Center(
+        //         child: CircularProgressIndicator(),
+        //       ),
       ),
 
       drawer: MyDrawer(),
