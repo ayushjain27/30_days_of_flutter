2cbdef65886fadb66fe48ef54953d0757e827d35
ayushjain27 <ayushjain201574@gmail.com>
2022-04-23 15:34:05 +0530

    Day-13 completed

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class MyTheme{
+class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
-          primarySwatch: Colors.deepPurple,
-          fontFamily: GoogleFonts.lato().fontFamily,
-          appBarTheme: const AppBarTheme(
-            color: Colors.white,
-            elevation: 0.0,
-            iconTheme: IconThemeData(color: Colors.black),
-            titleTextStyle: TextStyle(color: Colors.black, fontSize: 22),
-          ));
+      primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.poppins().fontFamily,
+      appBarTheme: const AppBarTheme(
+        color: Colors.white,
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
+        titleTextStyle: TextStyle(color: Colors.black, fontSize: 22),
+      ));
 
   static ThemeData DarkTheme(BuildContext context) => ThemeData(
-          brightness: Brightness.dark,
-  );
+        brightness: Brightness.dark,
+      );
+
+  // Colors
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
 }
