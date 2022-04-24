b95e661923e9e2de1761be4e6e4f86f6b62003df
ayushjain27 <ayushjain201574@gmail.com>
2022-04-24 12:39:00 +0530

    day-14 completed

---
+++ lib/widgets/home_widgets/catalog_header.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+import '../../widgets/themes.dart';
+
+
+class CatalogHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog App".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+        // Text("Catalog App"), // It is same as upper text
+        "Trending Products".text.xl2.make(),
+      ],
+    );
+  }
+}
